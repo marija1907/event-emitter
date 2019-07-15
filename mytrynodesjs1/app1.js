3c64a98166c595c787f316a3a454246a6f328864
Marija Redzepovic <marijadarkova@gmail.com>
2019-07-15 20:59:37 +0200

    Update app1.js

--- mytrynodesjs1/app1.js
+++ mytrynodesjs1/app1.js
@@ -1,12 +1,13 @@
 //var Emitt = require('./emitter1'); when we use file emitter1.js
 var Emitt = require('events');//without using emitter1.js file
+var configEvents = require('./config').events;//each events I have in application get properties in this object in this file
 var emit = new Emitt();
 
-emit.on('someMessage',function(){
+emit.on(configEvents.MESSAGE,function(){
     console.log('From somewhere...Hello!');
 });
-emit.on('someMessage',function(){
+emit.on(configEvents.MESSAGE,function(){
     console.log('This is message!');
 });
 console.log('Hello');
-emit.emit('someMessage');
+emit.emit(configEvents.MESSAGE);
